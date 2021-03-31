535ecb748f680f9c22042dd2f041dd9fd4513de7
janisyda <81559341+janisyda@users.noreply.github.com>
2021-03-31 11:43:37 +0530

    Add files via upload

--- data.php
+++ data.php
@@ -1,7 +1,7 @@
 <?php header('Content-Type: application/json');
 
 
-$servername = 'TO DO 1inventory-db-instance-1';
+$servername = 'inventory-db-instance-1';
 $username = 'master';
 $password = 'lab-password';
 $database = 'inventory';
